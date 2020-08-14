f5e8a694fff686a8f24b2ef97498c78b9dc29451
Claudio Laas <claudiolaas@gmail.com>
2020-08-14 18:01:14 -0400

    vid 69

--- todo/admin.py
+++ todo/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from .models import Todo
 
 # Register your models here.
-class TodoAdmin(admin.ModelAdmin):
+class TodoAdmin(admin.ModelAdmin): 
+    '''
+    customizes what the admin interface looks like 
+    for a particular model
+    '''
     readonly_fields = ('created',)
 
+
 admin.site.register(Todo,TodoAdmin)
+
+
+
+
+
